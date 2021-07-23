cfa255835b49da7d56323317e5e7c467c5276c57
Pellucid Dice <pelluciddice@gmail.com>
2021-07-24 09:50:08 +1200

    Fix lang error caused by the replacements

--- src/main/java/net/kaneka/planttech2/datagen/Languages.java
+++ src/main/java/net/kaneka/planttech2/datagen/Languages.java
@@ -8,11 +8,8 @@ import com.google.common.collect.ImmutableMap;
 import net.kaneka.planttech2.PlantTechMain;
 import net.kaneka.planttech2.crops.CropEntry;
 import net.kaneka.planttech2.registries.ModBlocks;
-import net.kaneka.planttech2.registries.ModEffects;
-import net.kaneka.planttech2.registries.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
-import org.apache.commons.lang3.StringUtils;
 
 import static net.kaneka.planttech2.PlantTechMain.MODID;
 import static net.kaneka.planttech2.registries.ModItems.*;
@@ -33,7 +30,7 @@ public class Languages extends LanguageProvider
         addGuides();
         addGUIs();
         addItems();
-        addCreativeModeTabs();
+        addItemGroups();
         addInfo();
         addMisc();
     }
@@ -73,15 +70,15 @@ public class Languages extends LanguageProvider
         add("temp.extreme_warm", "Extremely Warm");
     }
 
-    private void addCreativeModeTabs()
+    private void addItemGroups()
     {
-        add("CreativeModeTab.planttech2_main", "General");
-        add("CreativeModeTab.planttech2_blocks", "Building Blocks");
-        add("CreativeModeTab.planttech2_seeds", "Seeds");
-        add("CreativeModeTab.planttech2_machines", "Machines");
-        add("CreativeModeTab.planttech2_particles", "Particles");
-        add("CreativeModeTab.planttech2_toolsandarmor", "Tools and Armor");
-        add("CreativeModeTab.planttech2_chips", "Chips and Upgrades");
+        add("itemGroup.planttech2_main", "General");
+        add("itemGroup.planttech2_blocks", "Building Blocks");
+        add("itemGroup.planttech2_seeds", "Seeds");
+        add("itemGroup.planttech2_machines", "Machines");
+        add("itemGroup.planttech2_particles", "Particles");
+        add("itemGroup.planttech2_toolsandarmor", "Tools and Armor");
+        add("itemGroup.planttech2_chips", "Chips and Upgrades");
     }
 
     private void addItems()
